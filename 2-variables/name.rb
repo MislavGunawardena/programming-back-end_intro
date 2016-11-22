e109fdeabba7ee8995826de201ee2af38514f5ba
Mislav Gunawardena <shakthigster@gmail.com>
2016-11-22 09:48:59 +0000

    Completed Excercise4, modifies name.rb

--- 2-variables/name.rb
+++ 2-variables/name.rb
@@ -1,8 +1,11 @@
 
-puts "Please enter your name : "
-name = gets.chomp
-puts "Hello #{name}!, Hope you are having a wonderful day!"
+puts "Please enter your first name : "
+first_name = gets.chomp
+puts "Please enter your last name : "
+last_name = gets.chomp
+full_name = first_name + " " + last_name
+puts "Hello #{full_name}!, Hope you are having a wonderful day!"
 
 10.times do
-    puts name
+    puts full_name
 end
