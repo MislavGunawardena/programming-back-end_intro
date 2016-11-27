6a943031eb2f96c2f5e6944460e856076dc6a59c
Mislav Gunawardena <shakthigster@gmail.com>
2016-11-27 14:30:38 +0000

    Git status says that ex1.rb for excercises in arrays has been modified!

--- 6_arrays/ex1.rb
+++ 6_arrays/ex1.rb
@@ -1,7 +1,7 @@
 arr = [1,3,5,7,9,11]
 
 if arr.include?(3)
-    puts "3 is included in #{arr}"
+  puts "3 is included in #{arr}"
 else
-    puts "3 is not included in #{arr}"
+  puts "3 is not included in #{arr}"
 end
