84ff4a4cda819ee80c4e4a0a8fa0718016363790
Mislav Gunawardena <shakthigster@gmail.com>
2016-11-29 06:23:34 +0000

    Excercise 5 on hashes.

---
+++ Hashes/ex5.rb
@@ -0,0 +1,8 @@
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+if person.has_value?("painting")
+    puts "The person is into painting."
+else
+    puts "The person is not into painting."
+end
